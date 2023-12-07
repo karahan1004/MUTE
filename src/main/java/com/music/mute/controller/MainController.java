2326709a0ecb1e9051030df189dba28429e38295
Choe huijeong <chj06190306@gmail.com>
2023-12-07 14:57:14 +0900

    예시!

---
+++ src/main/java/com/music/mute/controller/MainController.java
@@ -0,0 +1,27 @@
+package com.music.mute.controller;
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/main", method = RequestMethod.GET)
+	public String home(Locale locale) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		
+		return "/main";
+	}
+	
+}
