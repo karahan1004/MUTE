f03e013ed8ca0e2ccbf4a064f8627bfc5bc80561
한정서 <jeongseohan1004@gmail.com>
2023-12-09 23:29:37 +0900

    로그인 화면과련 및  servlet-context.xml 컨텍스트명 변경

--- src/main/java/com/music/mute/login/LoginController.java
+++ src/main/java/com/music/mute/login/LoginController.java
@@ -1,5 +1,27 @@
 package com.music.mute.login;
 
-public class LoginController {
+import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String home(Locale locale) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		
+		return "/login";
+	}
+	
 }
