c82c5afc1921f73a97d72564413c82de2b9a8694
LeeChaeHee <brillar_23@naver.com>
2023-12-10 00:16:40 +0900

    테스트 페이지 UI작성 및 컨트롤러 연결

--- src/main/java/com/music/mute/test/TestController.java
+++ src/main/java/com/music/mute/test/TestController.java
@@ -1,5 +1,14 @@
 package com.music.mute.test;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TestController {
+	 @GetMapping("/test")
+	    public String showPlaylistPage() {
+	        return "test"; 
+	    }
 
 }
+
