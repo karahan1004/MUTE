f8b5f14e43a4cef8a02f1350ccf937b83d9d73ca
한정서 <jeongseohan1004@gmail.com>
2023-12-12 17:49:07 +0900

    main.jsp & main.css + AccessToken 관련 클래스

--- src/main/java/com/music/mute/login/LoginController.java
+++ src/main/java/com/music/mute/login/LoginController.java
@@ -2,16 +2,23 @@ package com.music.mute.login;
 
 import java.util.Locale;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+	
+	@Inject
+	private SpotifyService spotifyService;
+	
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -22,6 +29,15 @@ public class LoginController {
 		
 		
 		return "/login";
+	}
+	
+	@RequestMapping("/mute/main")
+	public String loginProcess(@RequestParam("code") String code) {
+		System.out.println("code>>>"+code);
+		String accessToken = spotifyService.requestAccessToken(code);
+		System.out.println("accessToken: "+accessToken);
+		
+		return "main";
 	}
 	
 	
