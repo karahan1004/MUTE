06b71f4f29389c11366d465e2e1ffc7ff800badc
한정서 <jeongseohan1004@gmail.com>
2023-12-18 00:10:27 +0900

    login UI 부분 수정

--- src/main/java/com/music/mute/login/SpotifyService.java
+++ src/main/java/com/music/mute/login/SpotifyService.java
@@ -20,11 +20,11 @@ public class SpotifyService {
     public URI generateAuthorizationUrl() {
         return spotifyApi.authorizationCodeUri()
                 .state("some-state")
-                .scope("user-read-private,user-read-email")
+                .scope("ugc-image-upload,user-read-playback-state,user-modify-playback-state,user-read-currently-playing,streaming,playlist-modify-public,user-library-modify")
                 //.showDialog(true)
                 .build()
                 .execute();
-    }
+    }/////
 
     public String requestAccessToken(String code) {
         try {
@@ -36,5 +36,26 @@ public class SpotifyService {
         } catch (IOException | SpotifyWebApiException | ParseException e) {
             throw new RuntimeException("Error requesting access token", e);
         }
-    }
-}
+    }//////
+    // refresh Token 받아와야 함!!!!!!// 
+//    public String requestRefreshToken(String refresh) {
+//    	try {
+//    		AuthorizationCodeRequest rreq = spotifyApi.authorizationCode(refresh)
+//    			.build();
+//    	
+//    		AuthorizationCodeCredentials authorizationCodeCredentials = rreq.execute();
+//			return authorizationCodeCredentials.getRefreshToken();
+//		} catch (ParseException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (SpotifyWebApiException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//    	
+//    }
+}//=========================
