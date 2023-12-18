bcfc848073be4357df546fbc02c4cff75e7535ba
jiheejiheekim <jihee2518@gmail.com>
2023-12-18 17:13:25 +0900

    OAuthController 추가

---
+++ src/main/java/com/music/mute/api/OAuthController.java
@@ -0,0 +1,65 @@
+package com.music.mute.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.Data;
+
+@Controller
+public class OAuthController {
+
+    private static final String CLIENT_ID = "61731dfa4f5a4f81a934c76fe09958d8";
+    private static final String CLIENT_SECRET = "7b9fbc7d7cb14319b7af63f5c5f18176";
+    private static final String REDIRECT_URI = "http://localhost:9089/mute/main";
+    private static final String SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize";
+    private static final String SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token";
+
+    @RequestMapping("/spotify/login")
+    public String login() {
+        // Redirect the user to the Spotify authorization URL
+        String redirectUrl = SPOTIFY_AUTH_URL +
+                "?client_id=" + CLIENT_ID +
+                "&response_type=code" +
+                "&redirect_uri=" + REDIRECT_URI +
+                "&scope=streaming user-read-email user-read-private user-library-read user-library-modify " +
+               "user-read-playback-state user-modify-playback-state playlist-read-collaborative " +
+               "user-read-currently-playing playlist-read-private playlist-modify-public " +
+               "playlist-modify-private";
+        return "redirect:" + redirectUrl;
+    }
+
+    @RequestMapping("/callback")
+    @ResponseBody
+    public String callback(@RequestParam("code") String code) {
+        // Exchange the authorization code for an access token
+        String tokenUrl = SPOTIFY_TOKEN_URL +
+                "?grant_type=authorization_code" +
+                "&code=" + code +
+                "&redirect_uri=" + REDIRECT_URI +
+                "&client_id=" + CLIENT_ID +
+                "&client_secret=" + CLIENT_SECRET;
+
+        RestTemplate restTemplate = new RestTemplate();
+        SpotifyAccessTokenResponse response = restTemplate.postForObject(tokenUrl, null, SpotifyAccessTokenResponse.class);
+
+        // Now you can use response.getAccessToken() to access Spotify API on behalf of the user
+
+        return "Success! Access Token: " + response.getAccess_token();
+    }
+
+    // Define a class to represent the response from the Spotify token endpoint
+    @Data
+    private static class SpotifyAccessTokenResponse {
+        private String access_token;
+        private String token_type;
+        private String expires_in;
+        private String refresh_token;
+        private String scope;
+
+        // Getters and setters...
+    }
+}
+
